7cfeaf9245f5b1ab6ec0a28efd277cd120fa54f9
SasankS007 <86741468+SasankS007@users.noreply.github.com>
2021-08-14 16:41:37 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+var background,sleep, brush, gym, eat, bath, move;
+var astronaut;
+
+function preload(){
+  background = loadImage("images/iss.png");
+  sleep = loadAnimation("images/sleep.png");
+  brush = loadAnimation("images/brush.png");
+  gym = loadAnimation("images/gym1.png","images/gym1.png","images/gym2.png","images/gym2.png");
+  eat = loadAnimation("images/eat1.png","images/eat1.png","images/eat1.png","images/eat2.png","images/eat2.png","images/eat2.png");
+  bath = loadAnimation("images/bath1.png","images/bath1.png","images/bath1.png","images/bath2.png","images/bath2.png","images/bath2.png");
+  move = loadAnimation("images/move1.png","images/move1.png","images/move2.png","images/move2.png");
+}
+
+function setup() {
+  createCanvas(600, 500);
+  
+  astronaut = createSprite(300,230);
+  astronaut.addAnimation("sleeping", sleep);
+  astronaut.scale = 0.1;
+  
+}
+
+function draw() {
+  background(background);
+  drawSprites();
+
+  textSize(20);
+  fill("white")
+  text("Instructions:",30, 30);
+  textSize(15);
+  text("Up Arrow = Brushing",30, 50);
+  text("Down Arrow = Gymming",30, 70);
+  text("Left Arrow = Eating",30, 80);
+  text("Right Arrow = Bathing",30, 100);
+  text("m key = Moving",30, 115);
+  
+  edges=createEdgeSprites();
+  astronaut.bounceOff(edges);
+  
+  if(keyDown("UP_ARROW")){
+
+  }
+
+
+
+
+    if(keyDown("DOWN_ARROW")){
+    astronaut.addAnimation("gymming", gym);
+    astronaut.changeAnimation("gymming");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+  
+  if(keyDown("LEFT_ARROW")){
+    astronaut.addAnimation("eating", eat);
+    astronaut.changeAnimation("eating");
+    astronaut.x = 150;
+    astronaut.y = 350;
+    astronaut.velocityX = 0.5;
+    astronaut.velocityY = 0.5;
+  }
+  
+  if(keyDown("RIGHT_ARROW")){
+    astronaut.addAnimation("bathing", bath);
+    astronaut.changeAnimation("bathing");
+    astronaut.x = 300;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+
+  if(keyDown("m")){
+    astronaut.addAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    astronaut.velocityX = 1;
+    astronaut.velocityY = 1;
+  }
+
+}
